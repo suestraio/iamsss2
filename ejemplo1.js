3bb11e88fa74790ca4d327519e7f648a08731d9e
Raul Hugo <info@suestra.io>
2018-08-09 09:15:38 -0500

    changing names

--- ejemplo1.js
+++
@@ -1,8 +0,0 @@
-var AWS = require('aws-sdk');
-AWS.config.region = 'us-east-2';
-
-var s3 = new AWS.S3();
-var params = {Bucket: 'videos.suestra.io', Key: 'acloudguru/acloudguru-servicios1.mp4'};
-s3.getSignedUrl('getObject', params, function (err, url) {
-  console.log("The URL is", url);
-});
